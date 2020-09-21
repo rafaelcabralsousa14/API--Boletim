6fa20a34c4670254cd0bd12a765b813507cfecf8
Rafael Cabral <rafacs1404@gmail.com>
2020-09-21 10:19:59 -0300

    Feito

---
+++ APIBoletim/Interfaces/iAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces
+{
+    interface iAluno
+    {
+        List<Aluno> ListarTodos();
+        Aluno BuscarPorID();
+        Aluno Cadastrar(Aluno a);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+    }
+}
---
+++ APIBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,40 @@
+﻿using APIBoletim.Context;
+using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using Microsoft.AspNetCore.Connections.Features;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : iAluno
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
